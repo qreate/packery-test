55d2b279b9458d4773c0d88d45a6b2e27f987c09
Robert Hjalmers <robert.hjalmers@gmail.com>
2014-09-21 21:12:02 +0200

    packery

--- directive/packery/packeryWallDir.js
+++ directive/packery/packeryWallDir.js
@@ -1,120 +1,77 @@
-angular.module('untitled').directive('packeryWallDir', function() {
-    return {
-        controller: [
-            '$scope',
-            '$element',
-            '$attrs',
-            function($scope, $element, $attrs){
-
-                var itemSelector,
-                    packeryOptions;
-
-                itemSelector = $attrs.packeryWallDir;
-
-                //this element will contain the packery
-                this.wallContainer = $element;
-
-                //we have some default options
-                //then overwrite with passed in options
-                //then overwrite with the necessary options
-                this.packeryOptions = _.assign(
-                    {},
-                    $scope.$eval($attrs.packeryWallOptions),
-                    {
-                        itemSelector: itemSelector,
-                    }
-                );
-
-                //place holder for packery to be setup and shared across all ng-repeat directive scopes
-                this.packery = new Packery(
-                    this.wallContainer[0],
-                    this.packeryOptions
-                );
-
-                this.packery.bindResize();
-                //this.packery.fit(element, x, y);
-
-
-                $scope.alert = function(id){
-                    //alert("hej")
-                    var currentSize = $scope.bricks[id].size;
-                    //alert($scope.bricks[id].size);
-                    switch(currentSize){
-                            case 'normal':
-                                $scope.bricks[id].size = 'wide';
-                                break;
-                            case 'wide':
-                                $scope.bricks[id].size = 'xwide';
-                                break;
-                            case 'xwide':
-                                $scope.bricks[id].size = 'normal';
-                                break;
-                    }
-                    //$scope.bricks[id].size =='wide'?'normal':'wide';
-                    //var target = document.getElementById('id0');
-                    //alert(target);
-                    this.$broadcast('resize');
-                    //this.$broadcast('resize',{element:target});
-                    //alert(this)
-                    //self.packery.fit();
-                };
-
-
-
-                var self = this;
-                this.debouncedReload = _.debounce(function(temp){
-                    console.log('I am only ran once after all the destroys are done!');
-                    console.log('item is being destroyed');
-                    //alert(temp)
-                    self.packery.reloadItems();
-                    self.packery.layout();
-                    //self.packery.fit(temp, 0,0);
-                }, 100);
-                this.fitMe = _.debounce(function(event){
-                    console.log('I am run!');
-                    //self.packery.reloadItems();
-                    self.packery.layout();
-                    //self.packery.fit(event.target);
-                },100);
-
-            }
-        ]
-    };
-}).directive('packeryItemDir',
-    function(){
+angular.module('untitled').directive('packeryWallDir', ['$rootScope', '$timeout',
+    function($rootScope, $timeout) {
         return {
+            restrict: 'A',
             scope: true,
-            require: '^packeryWallDir',
-            link: function(scope, element, attributes, packeryWallDirCtrl){
+            link: function(scope, element, attrs) {
+                //console.log("link called on", element[0]);
+                scope.element = element;
+                if (!$rootScope.packery) {
+                    $rootScope.packery = new Packery(element[0].parentElement, {
+                        gutter: 0,
+                        rowHeight: '.module-sizer',
+                        itemSelector: '.module',
+                        columnWidth: '.module-sizer'
+                    });
 
-                console.log('item is repeated');
+                    var draggable1 = new Draggabilly(element[0]);
+                    $rootScope.packery.bindDraggabillyEvents(draggable1);
 
-                imagesLoaded(element, function(){
-                    if(scope.$first){
-                        console.log('I get prepended');
-                        packeryWallDirCtrl.packery.prepended(element);
-                    }else{
-                        console.log('I get appended');
-                        packeryWallDirCtrl.packery.appended(element);
+                    var orderItems = function() {
+                        var itemElems = $rootScope.packery.getItemElements();
+                        $(itemElems).each(function(i, itemElem) {
+                            //$(itemElem).text(i + 1);
+                        });
+                    };
+
+                    $rootScope.packery.on('layoutComplete', orderItems);
+                    $rootScope.packery.on('dragItemPositioned', orderItems);
+
+
+                } else {
+                    // console.log("else", element[0]);
+                    $timeout(function() {
+                        $rootScope.packery.appended(element[0])
+                    });
+                    var draggable2 = new Draggabilly(element[0]);
+                    $rootScope.packery.bindDraggabillyEvents(draggable2);
+
+
+                }
+                $timeout(function() {
+                    $rootScope.packery.layout();
+                });
+
+
+                // watch for destroying an item
+                scope.$on('$destroy', function() {
+                    $rootScope.packery.remove(scope.element[0]);
+                    scope.packery.layout();
+                });
+
+                scope.resize = function(id){
+                    var currentSize = scope.windowItems[id].size;
+                    switch(currentSize){
+                        case 'normal':
+                            scope.windowItems[id].size = 'wide';
+                            break;
+                        case 'wide':
+                            scope.windowItems[id].size = 'full';
+                            break;
+                        case 'full':
+                            scope.windowItems[id].size = 'normal';
+                            break;
                     }
-                });
+                    //scope.windowItems[id].size = scope.windowItems[id].size == 'wide'?'normal':'wide';
+                    scope.$broadcast('resize');
 
-                scope.$on('$destroy', packeryWallDirCtrl.debouncedReload);
-                //scope.$on('resize', packeryWallDirCtrl.debouncedReload);
-                //scope.$on('resize', packeryWallDirCtrl.fitMe);
-                scope.$on('resize', function(event, args){
-                    //var myvalue = args.element;
-                    //var el = angular.element(document.getElementById('id1'));
-                    //alert(myvalue);
-                    packeryWallDirCtrl.debouncedReload();
-                    //packeryWallDirCtrl.debouncedReload(myvalue);
-                    //packeryWallDirCtrl.packery.fit(myvalue);
-                    //packeryWallDirCtrl.packery.layout();
+                };
+                scope.$on('resize', function(){
+                    $timeout(function() {scope.packery.fit(element[0])});
                 });
-                //scope.$on('resize', packeryWallDirCtrl.packery.fit(element));
-                //scope.$on('resize', packeryWallDirCtrl.packery.layout());
 
             }
         };
+
     }
-);
+]);
--- partial/packery/packery.js
+++ partial/packery/packery.js
@@ -1,44 +1,33 @@
-angular.module('untitled').controller('PackeryCtrl',function($scope){
-    function genBrick() {
-        var height = ~~(Math.random() * 500) + 100;
-        var id = ~~(Math.random() * 10000);
-        return {
-            src: 'http://lorempixel.com/g/280/' + height + '/?' + id,
-            title: 'hello-'+id,
-            size: 'normal',
-            id: 'id'
+angular.module('untitled').controller('PackeryCtrl', ['$scope', '$rootScope', '$timeout',
+    function($scope, $rootScope, $timeout) {
+
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        $scope.windowItems = [
+            { color: 'green', size: 'normal' },
+            { color: 'grey', size: 'normal' },
+            { color: 'lightgrey', size: 'normal' },
+            { color: 'lightgrey', size: 'normal' },
+            { color: 'lightgrey', size: 'normal' },
+            { color: 'lightgrey', size: 'normal' }
+        ];
+
+        $scope.add = function() {
+            $scope.windowItems.push({
+                color: getRandomColor()
+            });
         };
-    };
 
-    $scope.bricks = [
-        genBrick(),
-        genBrick(),
-        genBrick(),
-        genBrick(),
-        genBrick()
-    ];
+        $scope.remove = function() {
+            $scope.windowItems.pop();
+        };
 
-    $scope.add = function add() {
-        $scope.bricks.push(genBrick());
-    };
-
-    $scope.remove = function remove() {
-        $scope.bricks.splice(
-            ~~(Math.random() * $scope.bricks.length),
-            1
-        )
-    };
-    $scope.resize = function(id, size){
-        //alert($scope.toBeRepeated.indexOf(id))
-        //alert(id)
-        //alert($scope.toBeRepeated[id].isWide)
-        //$scope.bricks[id].size = $scope.bricks[id].size =='wide'?'normal':'wide';
-
-        //$scope.$broadcast('resize',{something:'myValue'});
-        //$scope.item[id].isWide = size =='wide'?'normal':'wide';
-    };
-    //$scope.alert = function alert(title) {
-    //  console.log("clicked: "+ title);
-    //masonry.layout();
-    //}
-});
+    }
+]);
